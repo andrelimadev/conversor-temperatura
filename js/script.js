9999473eb50a1e2520f9b6e513d859e9bdc08b23
andrelimadev <aslimadev@gmail.com>
2023-12-09 16:52:51 -0300

    quinto commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let input = document.querySelector("input").value;
+let input = document.querySelector("input");
 let celsius = document.querySelector(".celsius");
 let fahrenheit = document.querySelector(".fahrenheit");
 let temp1 = document.querySelector(".temp1");
 let temp2 = document.querySelector(".temp2");
 
-function cf(input = 0) {
+function cf() {
     let f = 0;
-    f = (input * 1.8) + 32;
+    f = (input.value * 1.8) + 32;
     temp1.innerHTML = "Celsius";
     temp2.innerHTML = "Fahrenheit";
-    celsius.innerHTML = `${input}°`;
+    celsius.innerHTML = `${input.value}°`;
     fahrenheit.innerHTML = `${f.toFixed(1)}°`;
 }
 
-function fc(input = 0) {
+function fc() {
     let c = 0;
-    c = (input - 32) / 1.8;
+    c = (input.value - 32) / 1.8;
     temp1.innerHTML = "Fahrenheit";
     temp2.innerHTML = "Celsius";
-    celsius.innerHTML = `${input}°`;
+    celsius.innerHTML = `${input.value}°`;
     fahrenheit.innerHTML = `${c.toFixed(1)}°`;
 }
